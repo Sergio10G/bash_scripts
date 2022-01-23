0c335ae3c2db7500c5284da0b0455fc35e47e09e
Sergio10G <sejobmx@hotmail.es>
2022-01-23 19:39:15 +0100

    start and stop xampp added

---
+++ start_xampp.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo /bin/bash /etc/init.d/apache2 stop
+sudo /bin/bash /opt/lampp/lampp start
---
+++ stop_xampp.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo /bin/bash /opt/lampp/lampp stop
+sudo /bin/bash /etc/init.d/apache2 start
