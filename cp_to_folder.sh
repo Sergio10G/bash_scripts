a17a4a658fb2efbbdfd0ac39d44354b22359589e
Sergio10G <sejobmx@hotmail.es>
2022-01-23 20:01:21 +0100

    cp_to_folder.sh now prints the folder it creates!

--- cp_to_folder.sh
+++ cp_to_folder.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Change the destination folder absolute path here.
-dest_folder="/home/testFolder"
+dest_folder="/home/sergiodg/Documentos/Test"
 cur_folder_name=${PWD##*/}
 
 cp_files_to_folder() {
-	[ -d "$dest_folder" ] && echo "$dest_folder exists!" || mkdir "$dest_folder"
+	[ -d "$dest_folder" ] && echo "$dest_folder exists!" || ( mkdir "$dest_folder" && echo "$dest_folder created!" )
 	cp -r ../$cur_folder_name $dest_folder/$cur_folder_name/ && echo "$cur_folder_name succesfully copied in $dest_folder/$cur_folder_name"
 }
 
