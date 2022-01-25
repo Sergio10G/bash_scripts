54b43c5dae4010981f74526f1f303f99827effcc
Sergio Diez garcia <sdiez-ga@c3r13s1.42madrid.com>
2022-01-25 18:56:58 +0100

    Now cp_to_folder needs a dest_folder parameter. Also more verbose.

--- cp_to_folder.sh
+++ cp_to_folder.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Change the destination folder absolute path here.
-dest_folder="/home/sergiodg/Documentos/Test"
+dest_folder=$1
 cur_folder_name=${PWD##*/}
 
 cp_files_to_folder() {
 	[ -d "$dest_folder" ] && echo "$dest_folder exists!" || ( mkdir "$dest_folder" && echo "$dest_folder created!" )
-	cp -r ../$cur_folder_name $dest_folder/$cur_folder_name/ && echo "$cur_folder_name succesfully copied in $dest_folder/$cur_folder_name"
+	[ -d "$dest_folder/$cur_folder_name" ] && cp -r ./* $dest_folder/$cur_folder_name/ && echo "Files copied to $dest_folder/$cur_folder_name succesfully!" || ( cp -r ../$cur_folder_name $dest_folder/$cur_folder_name/ && echo "$cur_folder_name succesfully copied in $dest_folder/$cur_folder_name" )
 }
 
 cp_files_to_folder
